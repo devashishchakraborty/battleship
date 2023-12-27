957df2bd19eee04201fc3f2168aaa4bdec3b7fd6
Devashish Chakraborty <devashishchakra@gmail.com>
2023-12-28 00:49:54 +0530

    Added a Gameboard Class with a board, placeShip and receiveAttack function. Also added a checkIfAllShipsSunk function to check whether all the ships are sunk.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,53 @@ class Ship{
         }
     }
 }
+
+class Gameboard{
+    constructor(){
+        this.carrier = new Ship(5);
+        this.battleship = new Ship(4);
+        this.destroyer = new Ship(3);
+        this.submarine = new Ship(3);
+        this.patrolBoat = new Ship(2);
+        this.board = [
+            ["","","","","","","","","",""],
+            ["","","","","","","","","",""],
+            ["","","","","","","","","",""],
+            ["","","","","","","","","",""],
+            ["","","","","","","","","",""],
+            ["","","","","","","","","",""],
+            ["","","","","","","","","",""],
+            ["","","","","","","","","",""],
+            ["","","","","","","","","",""],
+            ["","","","","","","","","",""],
+        ]
+    }
+
+    //  headCoords is the coordinates of the head of the ship.
+    //  orientation is either horizontal or vertical.
+    placeShip(ship, headCoords, orientation){
+        let [x, y] = headCoords;
+        for(let i = 0; i < ship.length; i++){
+            if(orientation === "horizontal")    this.board[x][y+i] = ship;
+            else if(orientation === "vertical") this.board[x+i][y] = ship;
+        }
+    }
+
+    receiveAttack(coords){
+        let [x, y] = coords;
+        if(typeof this.board[x][y] != "string"){
+            this.board[x][y].hit();
+            this.board[x][y] = "0"
+        } else {
+            this.board[x][y] = "x";
+        }
+    }
+    checkIfAllShipsSunk(){
+        this.board.forEach(function(row){
+            this.row.forEach(function(cell){
+                if(typeof cell != "string") return false;
+            })
+        })
+        return true;
+    }
+}
