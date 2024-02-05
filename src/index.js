fd7fd6407ef42cc2991cee95093ac1abe8a3c459
Devashish Chakraborty <devashishchakra@gmail.com>
2024-02-06 03:40:20 +0530

    Added a restart functionality to restart the game. Added Score Boards for each player. Also Added a winning Message at end. Finally modified a few more methods for better optimisation.

--- src/index.js
+++ src/index.js
@@ -35,14 +35,29 @@ class Gameboard {
         ]
     }
 
+
+    resetBoard(){
+        let newBoard = [];
+        for(let i = 0; i < 10; i++){
+            let row = [];
+            for(let j = 10; j < 10; j++){
+                row.push("");
+            }
+            newBoard.push(row);
+        }
+        this.board = newBoard;
+    }
+
+    
     getBoard() {
         return this.board;
     }
 
+
     isEmpty() {
-        return this.board.every((col) => {
-            return col.every((cell) => typeof cell === "string");
-        });
+        return this.board.every((row) => {
+            return row.every((cell) => typeof cell === "string")
+        })
     }
 
     //  headCoords is the coordinates of the head of the ship.
@@ -56,15 +71,6 @@ class Gameboard {
     receiveAttack([x, y]) {
         let shipObj = this.board[x][y];
         if (typeof shipObj === "object") shipObj.hit();
-    }
-
-    checkIfAllShipsSunk() {
-        this.board.forEach(function (row) {
-            row.forEach(function (cell) {
-                if (typeof cell != "string") return false;
-            })
-        })
-        return true;
     }
 
     randomlyPlaceShips() {
@@ -107,8 +113,8 @@ class Gameboard {
 
 
 class Player {
-    constructor(type) {
-        this.type = type;
+    constructor(name) {
+        this.name = name;
         this.score = 0;
         this.notShooted = this.totalCoords();
     }
@@ -149,8 +155,14 @@ class DOM {
         this.opponentGameboard = new Gameboard(); // computer Board
 
         // Players
-        this.player = new Player("human");
-        this.opponent = new Player("computer");
+        this.player = new Player("Player");
+        this.opponent = new Player("Opponent");
+
+        this.winMsg = document.querySelector(".winningMessage");
+
+        // Score Displays
+        this.playerScore = document.querySelector(".container .playerScore");
+        this.opponentScore = document.querySelector(".container .opponentScore");
 
         // Ships which are to be placed in the board itself.
         this.shipstoPlace = [
@@ -303,6 +315,7 @@ class DOM {
         }
     }
 
+
     activateStartGameButton() {
         const shipPlacingArea = document.querySelector(".shipPlacingArea");
         const startGameBtn = document.querySelector(".startGameBtn");
@@ -312,8 +325,9 @@ class DOM {
             shipPlacingArea.style.display = "none";
             mainGameSection.style.display = "flex";
             this.startMainGame();
-        });
+        }, { once: true });
     }
+
 
     populatePlayerGrid() {
         const playerGridCells = this.playerGrid.querySelectorAll("div");
@@ -328,6 +342,7 @@ class DOM {
             }
         })
     }
+
 
     populateOpponentGrid() {
         this.opponentGameboard.randomlyPlaceShips();
@@ -346,32 +361,32 @@ class DOM {
         });
     }
 
+
     shootOpponentBoard() {
         const opponentGridCells = this.opponentGrid.querySelectorAll("div");
         opponentGridCells.forEach((gridCell) => {
-            gridCell.addEventListener("mouseover", (e) => this.playerShootingHandler(e, opponentGridCells));
-            gridCell.addEventListener("mouseout", (e) => this.playerShootingHandler(e, opponentGridCells));
-            gridCell.addEventListener("click", (e) => this.playerShootingHandler(e, opponentGridCells));
+            gridCell.addEventListener("mouseover", (e) => this.playerShootingHandler(e));
+            gridCell.addEventListener("mouseout", (e) => this.playerShootingHandler(e));
+            gridCell.addEventListener("click", (e) => this.playerShootingHandler(e));
         });
     }
 
-    playerShootingHandler(event, opponentGridCells) {
+
+    playerShootingHandler(event) {
         if (!event.target.getAttribute("shot")) {
             if (event.type === "click") {
+                event.target.setAttribute("shot", "true");
                 if (event.target.getAttribute("type") === "ship") {
                     let row = event.target.getAttribute("row");
                     let col = event.target.getAttribute("col");
-                    let board = this.opponentGameboard.getBoard();
-                    let ship = board[row][col];
-                    ship.hit();
-
+                    this.updateScore(this.opponentGameboard, this.player, this.playerScore, [row, col]);
                 }
-                event.target.setAttribute("shot", "true");
                 this.shootPlayerBoard();
             }
             else event.target.setAttribute("event", event.type);
         }
     }
+
 
     shootPlayerBoard() {
         const playerGridCells = this.playerGrid.querySelectorAll("div");
@@ -380,14 +395,91 @@ class DOM {
 
         playerGridCells.forEach((cell) => {
             if (cell.getAttribute("row") === `${x}` && cell.getAttribute("col") === `${y}`) {
-                if (cell.getAttribute("type") === "ship") {
-                    board[x][y].hit();
-                }
                 cell.setAttribute("shot", "true");
+                if (cell.getAttribute("type") === "ship") {
+                    this.updateScore(this.playerGameboard, this.opponent, this.opponentScore, [x, y]);
+                }
             }
         })
-        this.shootOpponentBoard();
     }
+
+
+    updateScore(currentGameboard, currentPlayer, scoreBoard, [row, col]){
+        let board = currentGameboard.getBoard();
+
+        board[row][col].hit();
+        if (board[row][col].isSunk()) {
+            // Updating Score on board
+            currentPlayer.score += 1;
+            scoreBoard.textContent = `${currentPlayer.name}'s Score: ${currentPlayer.score}`
+        }
+    
+        board[row][col] = "";
+        // End Game
+        if (currentGameboard.isEmpty()) {
+            this.winMsg.textContent = `Game Over! ${currentPlayer.name} Win.`;
+
+            const opponentGridCells = this.opponentGrid.querySelectorAll("div");
+            opponentGridCells.forEach((cell) => {
+                cell.style.cursor = "default";
+                let clone = cell.cloneNode(true);
+                cell.parentNode.replaceChild(clone, cell);
+            })
+            this.activateRestartGameButton();
+        }
+    }
+
+
+    activateRestartGameButton(){
+        const shipPlacingArea = document.querySelector(".shipPlacingArea");
+        const mainGameSection = document.querySelector("main .container");
+        const restartGameBtn = document.querySelector(".restartGameBtn");
+        const startGameBtn = document.querySelector(".startGameBtn");
+
+        restartGameBtn.style.display = 'flex';
+        restartGameBtn.addEventListener("click", () => {
+            shipPlacingArea.style.display = "flex";
+            mainGameSection.style.display = "none";
+            restartGameBtn.style.display = "none";
+            startGameBtn.disabled = "true";
+            this.restart();
+            this.shipPlacement();
+        });
+    }
+
+
+    restart(){
+        this.shipPlacingGrid = this.createBoardGrid(document.querySelector(".shipPlacingArea .boardGrid"));
+        this.currentShipIcon = document.querySelector(".shipPlacingArea .currentShipIcon");
+
+        // Visual grids on screen
+        this.playerGrid = this.createBoardGrid(document.querySelector(".container .playerGrid"));
+        this.opponentGrid = this.createBoardGrid(document.querySelector(".container .opponentGrid"));
+
+        // Gridboard objects
+        this.playerGameboard.resetBoard();
+        this.opponentGameboard.resetBoard(); // computer Board
+
+        // Players
+        this.player = new Player("Player");
+        this.opponent = new Player("Opponent");
+
+        this.winMsg.textContent = "";
+
+        // Score Displays
+        this.playerScore.textContent = "Player Score: 0";
+        this.opponentScore.textContent = "Oppoenent Score: 0";
+
+        // Ships which are to be placed in the board itself.
+        this.shipstoPlace = [
+            new Ship("carrier", 5),
+            new Ship("battleship", 4),
+            new Ship("destroyer", 3),
+            new Ship("submarine", 3),
+            new Ship("patrolBoat", 2)
+        ]
+    }
+
 
     startMainGame() {
         this.populatePlayerGrid();    // To place ships on the Player Grid
